dae1f343aa5500e43a652f59c8cc45b002f264f1
Guilherme Augusto De Godoy Sachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2024-08-28 12:57:10 -0300

    feat: create screens navigation

--- src/routes/stack.routes.jsx
+++ src/routes/stack.routes.jsx
@@ -1,11 +1,21 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
+import Totoro from "../screens/Totoro";
 const Stack = createNativeStackNavigator();
 
 const StackRoutes = () => {
     return(
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen 
+            name="Home" 
+            component={Home} 
+            />
+
+            <Stack.Screen 
+            name="Totoro" 
+            component={Totoro} 
+            />
+
             </Stack.Navigator>
     );
 };
--- src/screens/Totoro/index.jsx
+++ src/screens/Totoro/index.jsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 
 export default function Totoro() {
+    const navigation = useNavigation()
   return (
     <View>
       <Text>my friend</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Text>Go to Home</Text>
+        </TouchableOpacity>
     </View>
   );
 };
